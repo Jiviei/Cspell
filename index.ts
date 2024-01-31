5013ea47490dbc4ff22557697574cd988357a794
Vasiliy Grigorev <vasiliy.grigorev@ics-it.ru>
2024-01-31 17:23:47 +0300

    update 31.01.24

--- index.ts
+++ index.ts
@@ -1,29 +1,40 @@
-import { getDefaultSettings, mergeSettings, readSettings, suggestionsForWord, validateText} from 'cspell-lib';
+import { getDefaultSettings, mergeSettings, readSettings, suggestionsForWord, validateText } from 'cspell-lib';
 
-const customWords = ['wordz', 'cuztom', 'clockz'];
-
-
-export const SpellcheckerFactory = async (customWords: string[] = []) => {
-    return async (text: string) => {
-        const settings = mergeSettings(await getDefaultSettings(false), await readSettings("@cspell\\dict-ru_ru\\cspell-ext.json"), await readSettings("@cspell\\dict-en-gb\\cspell-ext.json"))
-        return await validateText(text, {...settings, dictionaries: ["ru-ru", "en-gb"], words: ["coztom"]}, {
-            generateSuggestions: true,
-        });
-    };
-    return async (text: string) => {
-        const settings = mergeSettings(await getDefaultSettings(false), await readSettings("@cspell\\dict-ru_ru\\cspell-ext.json"))
-        return await suggestionsForWord(text, {
-            dictionaries: ["ru-ru"],
-        }, settings);
-    };
+export const SpellcheckerFactory = async (text: string, customdictionary: string[] = [], incorectdictionary: string[] = []) => {
+    const settings = mergeSettings(
+        await getDefaultSettings(false),
+        await readSettings("@cspell\\dict-ru_ru\\cspell-ext.json"),
+        await readSettings("@cspell\\dict-en-gb\\cspell-ext.json"))
+    return await validateText(text, {
+        ...settings,
+        minWordLength: 1,
+        dictionaries: ["ru-ru", "en-gb"],
+        ignoreWords: customdictionary,
+        flagWords:  [...incorectdictionary]
+    }, {
+        generateSuggestions: true,
+        numSuggestions: 5
+    });
 };
 
-export const checkSpelling = async (text: string) => {
-    const spellChecker = await SpellcheckerFactory(customWords);
-    return spellChecker(text);
-};
+interface answer {
+    text: string,
+    offset: number,
+    line: { text: string, offset: number },
+    isFlagged?: boolean,
+    suggestions?: string[],
+    suggestionsEx?: any[]
+}
+
 async function run() {
-    console.log('%o', await checkSpelling('привет праграмист world These are my coztom'));
+    const text = 'привет праграмист world These\n are my coztom wordz гитхаб пять ГГц Около ёж ёмкость емкость жжёт';
+    const customWords = ['wordz', 'cuztom', 'clockz'];
+    const reg = /\w*ё\w*/g
+    const incorectwords = ["пять: 5", "около"]
+    var answers: answer[] = await SpellcheckerFactory(text, customWords, incorectwords)
+    answers.map(answer => {
+        console.log(answer.text + ":", answer.suggestions)
+    });
 }
 
 run();
